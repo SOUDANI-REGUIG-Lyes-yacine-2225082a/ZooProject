e0379221e4ce1505074a0182a0f0d0d00f44fad3
SoudaniLyes <125369497+SOUDANI-REGUIG-Lyes-yacine-2225082a@users.noreply.github.com>
2023-11-17 12:09:47 +0100

    Add files via upload

---
+++ Animal/Oviparous/Dragon.java
@@ -0,0 +1,14 @@
+package Zoo.Animal.Oviparous;
+import Zoo.Animal.FlyingAnimal;
+
+public class Dragon extends Oviparous implements FlyingAnimal  {
+
+    public Dragon(String name, char sex, int weight, int size) {
+        super(name, sex, weight, size);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(super.getName() + " vole majestueusement !");
+    }
+}
