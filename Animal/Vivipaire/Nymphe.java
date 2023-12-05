530e4091adcdac6640552a6274c0ce793e10616d
SoudaniLyes <125369497+SOUDANI-REGUIG-Lyes-yacine-2225082a@users.noreply.github.com>
2023-12-05 22:29:30 +0100

    Add files via upload

--- Animal/Vivipaire/Nymphe.java
+++ Animal/Vivipaire/Nymphe.java
@@ -1,14 +1,26 @@
 package Animal.Vivipaire;
+import Animal.Animal;
 import Animal.AnimalTerrestre;
+import Animal.Resurrection;
 
-public class Nymphe extends Vivipaire implements AnimalTerrestre {
+public class Nymphe extends Vivipaire implements AnimalTerrestre, Resurrection {
 
-    public Nymphe(String name, char sex, int weight, int size) {
-        super(name, sex, weight, size);
-    }
+	public Nymphe(String name, char sex, int weight, int size) {
+		super(name, sex, weight, size);
+	}
 
-    @Override
-    public void walk() {
-        System.out.println(super.getName() + " se déplace gracieusement ");
-    }
+	@Override
+	public void walk() {
+		System.out.println(super.getName() + " se déplace gracieusement ");
+	}
+
+	@Override 
+	public String revive(Animal creature) {
+		if (!creature.isAlive()) {
+			return creature.getName() + " est en train de ressusciter";
+
+		} else {
+			return creature.getName() + " est vivant";
+		}
+	}
 }
