82af76f5ac940307487fda0eee334a1d2c2c8737
RayonPioct <120806298+RayonPioct@users.noreply.github.com>
2023-12-08 19:47:53 +0100

    Update AnimalVolant.java

--- Animal/AnimalVolant.java
+++ Animal/AnimalVolant.java
@@ -1,5 +1,14 @@
 package Animal;
 
+/**
+ * Interface représentant un animal volant, capable de voler.
+ */
 public interface AnimalVolant {
-    public String fly();
+
+    /**
+     * Méthode décrivant l'action de voler de l'animal volant.
+     *
+     * @return Une chaîne de caractères décrivant l'action de voler.
+     */
+    String fly();
 }
