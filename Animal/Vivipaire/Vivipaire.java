fa8e8511410e7eaa2088d85af30501bbc5a6e657
RayonPioct <120806298+RayonPioct@users.noreply.github.com>
2023-12-08 18:54:18 +0100

    vivipaire finit

--- Animal/Vivipaire/Vivipaire.java
+++ Animal/Vivipaire/Vivipaire.java
@@ -1,19 +1,40 @@
 package Animal.Vivipaire;
+
 import Animal.Animal;
 
+/**
+ * La classe Vivipaire représente une catégorie d'animaux vivipares.
+ * Elle étend la classe Animal.
+ */
 public abstract class Vivipaire extends Animal {
 
+    /**
+     * Constructeur de la classe Vivipaire.
+     *
+     * @param name   Le nom de l'animal vivipare.
+     * @param sex    Le sexe de l'animal vivipare.
+     * @param weight Le poids de l'animal vivipare.
+     * @param size   La taille de l'animal vivipare.
+     * @param age    L'âge de l'animal vivipare.
+     */
     public Vivipaire(String name, char sex, int weight, int size, int age) {
         super(name, sex, weight, size, age);
     }
 
+    /**
+     * Méthode permettant à l'animal vivipare de vagabonder.
+     */
     public void wander() {
         System.out.println(super.getName() + " vagabonde");
     }
 
+    /**
+     * Méthode permettant à l'animal vivipare de donner naissance à des petits.
+     * Affiche un message de félicitations si l'animal est de sexe femelle.
+     */
     public void giveBirth() {
-        if(super.getSex() == 'f') {
-            System.out.println("F�licitations ! " + super.getName() + " a mit bas !");
+        if (super.getSex() == 'f') {
+            System.out.println("Félicitations ! " + super.getName() + " a mis bas !");
         }
     }
 }
