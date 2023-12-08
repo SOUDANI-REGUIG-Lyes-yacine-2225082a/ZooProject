c7b94ff2a793f0830981fc2e8998e57605e325e7
RayonPioct <120806298+RayonPioct@users.noreply.github.com>
2023-12-08 19:55:09 +0100

    voliere javadoc

--- Enclos/Voliere.java
+++ Enclos/Voliere.java
@@ -2,10 +2,22 @@ package Enclos;
 
 import Animal.AnimalVolant;
 
-public class Voliere<T extends AnimalVolant> extends Enclosure {
+/**
+ * Classe représentant une volière destinée à héberger des animaux volants.
+ *
+ * @param <T> Type d'animal volant autorisé dans la volière.
+ */
+public class Voliere<T extends AnimalVolant> extends Enclosure<T> {
     private int height;
 
-
+    /**
+     * Constructeur de la classe Voliere.
+     *
+     * @param name      Nom de la volière.
+     * @param area      Superficie de la volière.
+     * @param maxAnimal Nombre maximum d'animaux que peut accueillir la volière.
+     * @param height    Hauteur de la volière.
+     */
     public Voliere(String name, int area, int maxAnimal, int height) {
         super(name, area, maxAnimal);
         this.height = height;
