8a215f4d420972b0a18509604d85e8fd97a00a1c
RayonPioct <120806298+RayonPioct@users.noreply.github.com>
2023-12-08 19:41:50 +0100

    FIX ANIMAL

--- Animal/Animal.java
+++ Animal/Animal.java
@@ -1,55 +1,36 @@
-java
-
 package Animal;
 
-/**
- * La classe abstraite Animal représente un être vivant dans le système du zoo.
- * Elle implémente l'interface Runnable pour permettre l'exécution asynchrone de certains comportements.
- * Cette classe forme la base pour la création d'objets spécifiques tels que les loups, licornes, nymphes, etc.
- * Les animaux ont des caractéristiques communes telles que le nom, le sexe, le poids, la taille, l'âge, la faim, etc.
- * Les actions possibles comprennent la gestion de la faim, l'alimentation, l'émission de sons, la guérison, la reproduction,
- * le sommeil, le réveil, le vieillissement, et d'autres.
- * Certains animaux peuvent être immortels, comme les dragons, les phénix, et les nymphes, et peuvent être ressuscités.
- * La classe inclut également un mécanisme de vieillissement avec un thread dédié.
- */
-public abstract class Animal implements Runnable {
-    private String name;
-    private char sex; // 'm' pour mâle, 'f' pour femelle
-    private int weight;
-    private int size; // en centimètres
-    private int age; // âge de l'animal
-    private int hunger; // faim sur 100
-    private boolean sleep; // FALSE = ne dort pas / TRUE = dort
-    private boolean health; // FALSE = mauvaise santé / TRUE = bonne santé
-
-    private boolean isAlive;
-
-    /**
-     * Constructeur de la classe Animal.
-     *
-     * @param name   Le nom de l'animal.
-     * @param sex    Le sexe de l'animal ('m' pour mâle, 'f' pour femelle).
-     * @param weight Le poids de l'animal.
-     * @param size   La taille de l'animal en centimètres.
-     * @param age    L'âge initial de l'animal.
-     */
-    public Animal(String name, char sex, int weight, int size, int age) {
-        this.name = name;
-        setSex(sex);
-        this.weight = weight;
-        this.size = size;
-        this.age = age;
-        this.hunger = 100;
-        this.sleep = false;
-        this.health = true;
-        this.isAlive = true;
-    }
 	
-	/**
- 	* Méthode pour gérer la faim de l'animal de manière asynchrone.
- 	* Diminue progressivement le niveau de faim de l'animal et déclenche des alertes de faim.
- 	* Marque l'animal comme mort s'il atteint un niveau de faim critique.
-	 */
+	// TODO : -add l'age fonct(viellissement, mort), -Revive des immortels(dragons, phoenixs, nymphes)
+	// TODO : -add la période d'incubation ou gestation
+
+    public abstract class Animal implements Runnable{
+	private String name;
+	private char sex; // m : male, f : female
+	private int weight;
+	private int size; // en centimetres
+	private int age; // age de l'animal
+	private int hunger; // faim sur 100
+	private boolean sleep; // FALSE = ne dort pas / TRUE = dort
+	private boolean health; // FALSE = mauvaise sante / TRUE = bonne sante
+
+	private boolean isAlive;
+
+	public Animal(String name, char sex, int weight, int size, int age) {
+		this.name = name;
+		setSex(sex);
+		this.weight = weight;
+		this.size = size;
+		this.age = age;
+		this.hunger = 100;
+		this.sleep = false;
+		this.health = true;
+	}
+	
+	
+	//elements avec thread
+	
+	//gestion de la faim
 	public void hunger() {
 		while (this.hunger > 0 && !Thread.interrupted()) {
 			try {
@@ -71,47 +52,32 @@ public abstract class Animal implements Runnable {
 			}
 		}
 	}
+	
 
-	/**
-	* Implémentation de l'interface Runnable pour gérer de manière asynchrone le niveau de faim de l'animal.
- 	* La méthode {@code run()} appelle la méthode {@code hunger()} pour diminuer la faim de l'animal progressivement.
- 	*/
 	@Override
 	public void run() {
-    		hunger();
+		hunger();
 	}
-
-	/**
- 	* Méthode permettant à l'animal de se nourrir, augmentant son niveau de faim.
- 	* Affiche un message indiquant que l'animal a été nourri et met à jour le niveau de faim.
- 	* Ne permet pas de nourrir l'animal s'il est endormi ou si son niveau de faim est déjà au maximum.
-	*/
+	
 	public void eat() {
-    	if (isAlive) {
-        	if (hunger < 100 && !sleep) {
-            		hunger = hunger + 50;
-            		System.out.println("Vous nourrissez " + name + " et sa faim est maintenant à " + hunger);
-        	} else {
-            		System.out.println("Vous ne pouvez pas nourrir " + name + " maintenant");
-        	}
-    	}
+		if (isAlive) {
+			if (hunger < 100 && !sleep) {
+				hunger = hunger + 50;
+				System.out.println("Vous nourrissez " + name + " et sa faim et maintenant à " + hunger);
+			} else {
+				System.out.println("Vous ne pouvez pas nourrir " + name + " maintenant");
+			}
+		}
 	}
 
-	/**
- 	* Méthode permettant à l'animal d'émettre un son.
- 	* Affiche un message indiquant que l'animal émet un son.
- 	*/
 	public void sound() {
-    		System.out.println(this.name + " émet un son");
+		System.out.println(this.name + " emet un son");
 	}
 
-	/**
- 	* Méthode permettant à l'animal de guérir. Si la santé de l'animal était mauvaise, elle est rétablie à bonne santé.
-	*/
 	public void heal() {
-    		if (!this.health) {
-        	    this.health = true;
-    		}
+		if(!this.health) {
+			this.health = true;
+		}
 	}
 
 	public void giveBirth() {
@@ -224,4 +190,3 @@ public abstract class Animal implements Runnable {
 				'}';
 	}
 }
-
