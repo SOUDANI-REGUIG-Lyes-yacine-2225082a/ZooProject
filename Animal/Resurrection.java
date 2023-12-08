96139f474ebe02a938a252a93d6a644a831fc171
RayonPioct <120806298+RayonPioct@users.noreply.github.com>
2023-12-08 19:48:40 +0100

    Update Resurrection.java

--- Animal/Resurrection.java
+++ Animal/Resurrection.java
@@ -1,6 +1,15 @@
 package Animal;
 
+/**
+ * Interface représentant la capacité de résurrection d'un animal.
+ */
 public interface Resurrection {
-	public String revive(Animal animal);
-	//..
+
+    /**
+     * Méthode permettant à l'animal de ressusciter.
+     *
+     * @param animal L'animal à ressusciter.
+     * @return Une chaîne de caractères indiquant le résultat de la résurrection.
+     */
+    String revive(Animal animal);
 }
